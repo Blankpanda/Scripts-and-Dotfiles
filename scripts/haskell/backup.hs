7dc65d6a49d0f041e0d3c9035487939b4a4fde4a
blankpanda <blankpanda8@gmail.com>
2016-08-20 11:41:08 -0400

    backup script

---
+++ scripts/haskell/backup.hs
@@ -0,0 +1,26 @@
+import System.Environment
+import System.IO
+
+readArgFile :: String -> IO Handle -- read file arguement
+readArgFile path = openFile path ReadMode 
+
+createBackup :: String -> IO Handle -- create a new file
+createBackup path = openFile path WriteMode
+
+main = do args <- getArgs
+          oldFileHandle <- readArgFile (head args)
+          backupFileHandle <- createBackup ((head args) ++ ".bak")
+          contents <- hGetContents oldFileHandle
+          putStr contents
+          hPutStrLn backupFileHandle contents
+          
+          hClose oldFileHandle
+          hClose backupFileHandle
+
+          
+
+
+
+
+    
+
